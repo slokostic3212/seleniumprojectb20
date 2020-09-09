db28fa28f11a0337d3b8439ef1e8cba3618d98f0
slokostic3212 <dsk3212@yahoo.com>
2020-09-09 10:47:05 -0400

    update day3_cssSelector-xpath

--- src/test/java/com/cybertek/tests/day3_cssSelector_xpath/P4_PracticeCybertek_Xpath.java
+++ src/test/java/com/cybertek/tests/day3_cssSelector_xpath/P4_PracticeCybertek_Xpath.java
@@ -8,15 +8,13 @@ import org.openqa.selenium.WebElement;
 public class P4_PracticeCybertek_Xpath {
 
     public static void main(String[] args) {
-        public static void main(String[] args) {
-            WebDriver driver = WebDriverFactory.getDriver("chrome");
-            driver.get("http://practice.cybertekschool.com/multiple_buttons");
+        WebDriver driver = WebDriverFactory.getDriver("chrome");
+        driver.get("http://practice.cybertekschool.com/multiple_buttons");
 
-            //locate button 2 and click on it
-            //only using xpath
+        //locate button 2 and click on it
+        //only using xpath
 
-            WebElement button2 = driver.findElement(By.xpath("//button[@name='button2'] "));
-            button2.click();
+        WebElement button2 = driver.findElement(By.xpath("//button[@name='button2'] "));
+        button2.click();
     }
-
 }
