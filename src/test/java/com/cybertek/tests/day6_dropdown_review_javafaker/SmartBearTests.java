6bf2a0d89c7dcf40a0f26454880521f7cb075471
slokostic3212 <dsk3212@yahoo.com>
2020-09-20 18:45:22 -0400

    update SmartBearTests.java

--- src/test/java/com/cybertek/tests/day6_dropdown_review_javafaker/SmartBearTests.java
+++ src/test/java/com/cybertek/tests/day6_dropdown_review_javafaker/SmartBearTests.java
@@ -1,5 +1,6 @@
 package com.cybertek.tests.day6_dropdown_review_javafaker;
 
+import com.cybertek.utilities.SmartBearUtilities;
 import com.cybertek.utilities.WebDriverFactory;
 import com.github.javafaker.Faker;
 import org.openqa.selenium.By;
@@ -15,19 +16,11 @@ import java.util.List;
 import java.util.concurrent.TimeUnit;
 
 public class SmartBearTests {
-    //TC #1: Smartbear software link verification
-    // 1.Open browser
-    // 2.Go to website: http://secure.smartbearsoftware.com/samples/testcomplete12/WebOrders/login.aspx
-    // 3.Enter username: “Tester”
-    // 4.Enter password: “test”
-    // 5.Click to Login button
-    // 6.Print out count of all the links on landing page
-    // 7.Print out each link text on this page
 
     WebDriver driver;
 
     @BeforeMethod
-    public void setupMethod() {
+    public void setupMethod(){
         driver = WebDriverFactory.getDriver("chrome");
         //maximize the page
         driver.manage().window().maximize();
@@ -35,23 +28,13 @@ public class SmartBearTests {
         driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
         //get the page
         driver.get("http://secure.smartbearsoftware.com/samples/testcomplete12/WebOrders/login.aspx");
+
+        SmartBearUtilities.loginToSmartBear(driver);
     }
 
 
     @Test
-    public void task1_login_page_links_print_test() {
-        //3. Enter username: “Tester”
-        WebElement usernameInput = driver.findElement(By.id("ctl00_MainContent_username"));
-        usernameInput.sendKeys("Tester");
-
-        //4. Enter password: “test”
-        WebElement passwordInput = driver.findElement(By.id("ctl00_MainContent_password"));
-        passwordInput.sendKeys("test");
-
-        //5. Click to Login button
-        WebElement loginButton = driver.findElement(By.id("ctl00_MainContent_login_button"));
-        loginButton.click();
-
+    public void task1_login_page_links_print_test(){
         //6. Print out count of all the links on landing page
         List<WebElement> allLinks = driver.findElements(By.xpath("//body//a"));
 
@@ -59,14 +42,14 @@ public class SmartBearTests {
 
         //7. Print out each link text on this page
 
-        for (WebElement eachLink : allLinks) {
+        for(WebElement eachLink : allLinks){
             System.out.println("eachLink = " + eachLink.getText());
         }
 
     }
 
     @Test
-    public void task2_create_order_with_javafaker(){
+    public void task2_create_order_with_javafaker() throws InterruptedException {
         //6. Click on Order
         WebElement orderLink = driver.findElement(By.xpath("//a[.='Order']"));
         orderLink.click();
@@ -85,15 +68,14 @@ public class SmartBearTests {
 
         //8. Click to “Calculate” button
         WebElement calculateButton = driver.findElement(By.xpath("//input[@value='Calculate']"));
+        calculateButton.click();
 
         //locating all of the web elements using id
-
         WebElement nameInput = driver.findElement(By.id("ctl00_MainContent_fmwOrder_txtName"));
         WebElement streetInput = driver.findElement(By.id("ctl00_MainContent_fmwOrder_TextBox2"));
         WebElement cityInput = driver.findElement(By.id("ctl00_MainContent_fmwOrder_TextBox3"));
         WebElement stateInput = driver.findElement(By.id("ctl00_MainContent_fmwOrder_TextBox4"));
         WebElement zipInput = driver.findElement(By.id("ctl00_MainContent_fmwOrder_TextBox5"));
-
 
         //9. Fill address Info with JavaFaker
         Faker faker = new Faker();
@@ -109,11 +91,12 @@ public class SmartBearTests {
         zipInput.sendKeys(faker.address().zipCode().replaceAll("-",""));
 
         //10. Click on “visa” radio button
-        WebElement visaRadio = driver.findElement(By.id("ct100_MainContent_fmwOrder_cardList_0"));
+        WebElement visaRadio = driver.findElement(By.id("ctl00_MainContent_fmwOrder_cardList_0"));
+        visaRadio.click();
 
         //11. Generate card number using JavaFaker
-        WebElement cardNumber = driver.findElement(By.id("ctl00_MainContent_fmwOrder_TextBox6"));
-        cardNumber.sendKeys(faker.finance().creditCard());
+        WebElement creditCardInput = driver.findElement(By.id("ctl00_MainContent_fmwOrder_TextBox6"));
+        creditCardInput.sendKeys(faker.finance().creditCard().replaceAll("-", ""));
 
         //Enter expiration date
         WebElement expirationInput = driver.findElement(By.id("ctl00_MainContent_fmwOrder_TextBox1"));
@@ -123,16 +106,9 @@ public class SmartBearTests {
         WebElement processButton = driver.findElement(By.id("ctl00_MainContent_fmwOrder_InsertButton"));
         processButton.click();
 
-        //13.Verify success message “New order has been successfully added.”
+        //13.Verify success message “New order has been successfully added.” is displayed.
         WebElement successMessage = driver.findElement(By.xpath("//strong"));
         Assert.assertTrue(successMessage.isDisplayed(), "Success message is not displayed!");
-
-        //Mini-Task:
-        //#1- Create a new class called : SmartBearUtilities
-        //#2- Create a static method called loginToSmartBear
-        //#3- This method simply logs in to SmartBear when you call it.
-        //#4- Accepts WebDriver type as parameter
-
     }
 }
 
