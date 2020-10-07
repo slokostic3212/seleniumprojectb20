b0218bc19522a4850291b771421ebffab6c872ef
slokostic3212 <dsk3212@yahoo.com>
2020-10-06 22:06:15 -0400

    update day11_utils_review_actions

--- src/test/java/com/cybertek/tests/day11_utils_review_actions/ActionsPractices.java
+++ src/test/java/com/cybertek/tests/day11_utils_review_actions/ActionsPractices.java
@@ -10,18 +10,18 @@ import org.testng.annotations.Test;
 
 public class ActionsPractices {
     /*
-    TC #15: Hover Test
-    1. Go to http://practice.cybertekschool.com/hovers
-    2. Hover over to first image
-    3. Assert:
-        a. “name: user1” is displayed
-    4. Hover over to second image
-    5. Assert:
-        a. “name: user2” is displayed
-    6. Hover over to third image
-    7. Confirm:
-        a. “name: user3” is displayed
-     */
+   TC #15: Hover Test
+   1. Go to http://practice.cybertekschool.com/hovers
+   2. Hover over to first image
+   3. Assert:
+       a. “name: user1” is displayed
+   4. Hover over to second image
+   5. Assert:
+       a. “name: user2” is displayed
+   6. Hover over to third image
+   7. Confirm:
+       a. “name: user3” is displayed
+    */
     @Test
     public void hover_over_test(){
         //driver
@@ -56,7 +56,5 @@ public class ActionsPractices {
         Assert.assertTrue(user3.isDisplayed(), "User 3 is NOT displayed! Verification failed!!!");
 
 
-
     }
-
 }
