4e33f665b30668af68a5d2161aa1e35f46ebf427
slokostic3212 <dsk3212@yahoo.com>
2020-10-13 21:53:41 -0400

    update pages

---
+++ src/test/java/com/cybertek/pages/LoginPage.java
@@ -0,0 +1,19 @@
+package com.cybertek.pages;
+
+import com.cybertek.utilities.Driver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+public class LoginPage {
+    public LoginPage() {
+
+        PageFactory.initElements(Driver.getDriver(), this);
+    }
+
+    @FindBy(id = "ct100_MainContent_username")
+    public WebElement usernameInput;
+
+
+
+}
