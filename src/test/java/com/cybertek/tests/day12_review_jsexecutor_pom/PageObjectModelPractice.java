7d682dd0bd3a9e4e7493020d40e958dfb88577bd
slokostic3212 <dsk3212@yahoo.com>
2020-10-14 19:52:25 -0400

    update PageObjectModelPractice

--- src/test/java/com/cybertek/tests/day12_review_jsexecutor_pom/PageObjectModelPractice.java
+++ src/test/java/com/cybertek/tests/day12_review_jsexecutor_pom/PageObjectModelPractice.java
@@ -5,6 +5,9 @@ import org.testng.annotations.Test;
 
 public class PageObjectModelPractice {
 
+    LoginPage loginPage;
+    //LandingPage landingPage;
+
     @Test
     public void login_to_smartbear(){
         LoginPage loginPage = new LoginPage();
