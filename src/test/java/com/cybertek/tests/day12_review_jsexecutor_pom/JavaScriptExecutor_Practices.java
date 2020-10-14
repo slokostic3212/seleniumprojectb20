3b03f417188d24a2573226b760b93a43c000773f
slokostic3212 <dsk3212@yahoo.com>
2020-10-14 18:47:27 -0400

    update day12_review_jsexecutor_pom

--- src/test/java/com/cybertek/tests/day12_review_jsexecutor_pom/JavaScriptExecutor_Practices.java
+++ src/test/java/com/cybertek/tests/day12_review_jsexecutor_pom/JavaScriptExecutor_Practices.java
@@ -8,7 +8,6 @@ import org.openqa.selenium.WebElement;
 import org.testng.annotations.Test;
 
 public class JavaScriptExecutor_Practices {
-
     @Test
     public void scroll_using_jsexecutor_1(){
         //Get the page to scroll
@@ -34,7 +33,7 @@ public class JavaScriptExecutor_Practices {
         Driver.getDriver().get("http://practice.cybertekschool.com/large");
 
         //locating cybertek school link
-        WebElement link = Driver.getDriver().findElement(By.linkText("Cybertek School"));
+        WebElement cybertekLink = Driver.getDriver().findElement(By.linkText("Cybertek School"));
         WebElement homeLink = Driver.getDriver().findElement(By.linkText("Home"));
 
         //use js executor to scroll 'Cybertek School' link intoView
@@ -42,9 +41,38 @@ public class JavaScriptExecutor_Practices {
 
         //use scrollIntoView function from JavaScript to scroll to a specific web element
         BrowserUtils.wait(2);
-        js.executeScript("arguments[0].scrollIntoView(true)", link);
+        js.executeScript("arguments[0].scrollIntoView(true)", cybertekLink);
         BrowserUtils.wait(2);
         js.executeScript("arguments[0].scrollIntoView(true)", homeLink);
+
+        BrowserUtils.wait(2);
+        js.executeScript("window.scrollBy(500, 500)");
+
+    }
+
+    @Test
+    public void fill_form_using_javascript(){
+        Driver.getDriver().get("http://practice.cybertekschool.com/sign_up");
+
+        WebElement usernameInput = Driver.getDriver().findElement(By.name("full_name"));
+        WebElement emailInput = Driver.getDriver().findElement(By.name("email"));
+        WebElement signUpbutton = Driver.getDriver().findElement(By.name("wooden_spoon"));
+
+        BrowserUtils.wait(1);
+
+        JavascriptExecutor js = (JavascriptExecutor) Driver.getDriver();
+
+        //sending keys to usernameInput using JavaScript function .setAttribute
+        js.executeScript("arguments[0].setAttribute('value', 'Jane Doe')", usernameInput);
+
+        BrowserUtils.wait(1);
+        //sending keys to emailInput using JavaScript function .setAttribute
+        js.executeScript("arguments[0].setAttribute('value', 'something@gmail.com')", emailInput);
+
+        BrowserUtils.wait(1);
+        //clicking to signUpbutton using JavaScript function
+        js.executeScript("arguments[0].click()", signUpbutton);
+
 
 
 
